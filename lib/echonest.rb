9408d78a282c139f99f6103457fdc2b9bb2b6998
Mike Risse <rissem@gmail.com>
2012-02-11 14:45:47 -0800

    some echonest seatwave integration

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -20,5 +20,18 @@ module Echonest
       response =  api("song/search", {results: 1, artist:artist, title:title, limit:true})
       return response["response"]["songs"][0]
     end
+
+    def self.upcoming_events(city)
+      response = JSON.parse(open("http://api-sandbox.seatwave.com/v2/discovery/events?apikey=4739E4694D0E482A92C9D0B478D83934&what=music&where=#{URI.escape(city)}").read)
+      events = response["Events"]
+      events[0..5].each do |event|
+        puts event["EventGroupId"]
+        puts find_artst_by_seatwave_id(event["EventGroupId"])
+      end
+    end
+
+    def self.find_artst_by_seatwave_id(seatwave_id)
+      api("artist/profile", {id: "seatwave:artist:#{seatwave_id}", bucket:"biographies"})
+    end
   end
 end
