e041d4b0a2503baf060225e011468f43ef3a9396
Mike Risse <rissem@gmail.com>
2012-02-12 10:14:52 -0800

    'prettier' events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   def index
     @events = Event.within params[:city]
+    @events.sort! do |e1, e2|
+      e1["Date"].<=>(e2["Date"])
+    end
+    @city = params[:city]
   end
 
   def show
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
+require 'date'
+
 module EventsHelper
+  def convert(weird_string)
+    ms = weird_string.split("+")[0]
+    ms = ms.split("(")[1]
+    return DateTime.strptime(ms[0..-4], '%s')
+  end
 end
