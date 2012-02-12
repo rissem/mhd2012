b5d5ab58d9bffa2c8e7081552e724723451cc4f6
Mike Risse <rissem@gmail.com>
2012-02-11 16:47:19 -0800

    modify echonest lib to allow passing multiple buckets

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -9,6 +9,12 @@ module Echonest
     def self.api(noun, params, debug=false)
       query_params = ""
       params.each do |key,value|
+        if value.kind_of?(Array)
+          value.each do |x|
+            query_params = "#{query_params}&#{URI.escape(key.to_s)}=#{URI.escape(x.to_s)}"
+          end
+          next
+        end
         query_params = "#{query_params}&#{URI.escape(key.to_s)}=#{URI.escape(value.to_s)}"
       end
       url = "http://developer.echonest.com/api/v4/#{noun}?api_key=#{API_KEY}&format=json&bucket=id:rdio-us-streaming#{query_params}"
@@ -24,7 +30,7 @@ module Echonest
     end
 
     def self.find_artist_by_seatwave_id(seatwave_id)
-      api("artist/profile", {id: "seatwave:artist:#{seatwave_id}", bucket:"biographies"})
+      api("artist/profile", {id: "seatwave:artist:#{seatwave_id}", bucket:["blogs","biographies", "familiarity", "hotttnesss", "images", "news", "reviews"]}, false)
     end
   end
 end
